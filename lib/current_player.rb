4dc4d7205b5ab3b09bc4f65d9457ad786addb6d3
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-14 19:11:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   board.each do |position|
   occupied_positions =0
   if position == "X" || position == "O"
-        occupied_positions  +=
+        occupied_positions  += 1
       return occupied_positions
       puts occupied_positions
     end
